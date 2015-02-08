f81d87b3a576bdfdc89ef96a7492cf879e3b1e82
Ted Ralphs <ted@lehigh.edu>
2015-02-08 17:13:09 +0000

    temporarily committing stable candidate

--- DyLP/src/DylpStdLib/config_dylp_default.h
+++ DyLP/src/DylpStdLib/config_dylp_default.h
@@ -5,13 +5,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define DYLP_VERSION  "trunk"
+#define DYLP_VERSION "1.10"
 
 /* Major Version number of project */
-#define DYLP_VERSION_MAJOR   9999
+#define DYLP_VERSION_MAJOR 1
 
 /* Minor Version number of project */
-#define DYLP_VERSION_MINOR   9999
+#define DYLP_VERSION_MINOR 10
 
 /* Release Version number of project */
 #define DYLP_VERSION_RELEASE 9999
